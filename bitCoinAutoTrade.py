e7a2e3e4130d3f8f0366d0c6b112ef41189c23cf
redPop34 <83233848+redPop34@users.noreply.github.com>
2021-04-29 20:18:03 +0900

    Update bitCoinAutoTrade.py

--- bitCoinAutoTrade.py
+++ bitCoinAutoTrade.py
@@ -36,7 +36,9 @@ upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
 lp = 1.02
-bisSelled = False
+bisSelled = True
+sleepTime = 1
+bisFinished = False
 # 자동매매 시작
 while True:
     try:
@@ -45,13 +47,16 @@ while True:
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.7)
+            target_price = get_target_price("KRW-BTC", 0.5)
             current_price = get_current_price("KRW-BTC")
+
             if target_price < current_price and bisSelled == False:
                 krw = get_balance("KRW")
-                if krw > 5000:                    
+                if krw > 5000 and bisFinished == False:                    
                     upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    sleepTime = 5
                 else:
+                    bisFinished = True
                     downLimitPrice = target_price * (lp-0.02)
                     upLimitPrice = target_price * lp                    
                     if upLimitPrice < current_price:
@@ -59,16 +64,19 @@ while True:
                     elif downLimitPrice > current_price and lp > 1.02:
                         bisSelled = True
                         lp = 1.02
+                        sleepTime = 1
                         btc = get_balance("BTC")
                         if btc > 0.00008:
                             upbit.sell_market_order("KRW-BTC", btc*0.9995)
         else:
             bisSelled = False
+            bisFinished = False
+            sleepTime = 1
             lp = 1.02
             btc = get_balance("BTC")
             if btc > 0.00008:
                 upbit.sell_market_order("KRW-BTC", btc*0.9995)
-        time.sleep(1)
+        time.sleep(sleepTime)
     except Exception as e:
         print(e)
         time.sleep(1)
