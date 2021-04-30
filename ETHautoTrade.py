6441fc01b404699d2f8167796c6457a281f1b7e3
redPop34 <83233848+redPop34@users.noreply.github.com>
2021-04-30 16:38:41 +0900

    Update ETHautoTrade.py

--- ETHautoTrade.py
+++ ETHautoTrade.py
@@ -1,6 +1,7 @@
 import time
 import pyupbit
 import datetime
+import logging
 
 access = "dZWTThOKBIDlTXutNuFgTSQSBJxTJhQ83W03iyxs"
 secret = "HEWRGm9rfV43x7j4rM2s48JGnj05K7sKwEVoQaGh"
@@ -31,9 +32,28 @@ def get_current_price(ticker):
     """현재가 조회"""
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
+# 로그 생성
+logger = logging.getLogger()
+
+# 로그의 출력 기준 설정
+logger.setLevel(logging.INFO)
+
+# log 출력 형식
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# log 출력
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+# log를 파일에 출력
+file_handler = logging.FileHandler('BTC.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
-print("autotrade start")
+logger.info("autotrade start")
 
 lp = 1.02
 bisSelled = True
@@ -43,40 +63,46 @@ bisFinished = False
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-ETH")
+        start_time = get_start_time("KRW-BTC")
         end_time = start_time + datetime.timedelta(days=1)
-
+        
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-ETH", 0.5)
-            current_price = get_current_price("KRW-ETH")
+            target_price = get_target_price("KRW-BTC", 0.5)
+            current_price = get_current_price("KRW-BTC")
 
             if target_price < current_price and bisSelled == False:
+                logger.info("cross price")
                 krw = get_balance("KRW")
                 if krw > 5000 and bisFinished == False:                    
-                    upbit.buy_market_order("KRW-ETH", krw*0.9995)
-                    sleepTime = 5
+                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    logger.info("buy BTC")
+                    sleepTime = 3
                 else:
                     bisFinished = True
                     downLimitPrice = target_price * (lp-0.02)
                     upLimitPrice = target_price * lp                    
                     if upLimitPrice < current_price:
                         lp = lp + 0.02
-                    elif downLimitPrice > current_price and lp > 1.02:
+                    elif float(downLimitPrice) > float(current_price) and float(lp) > 1.02:
                         bisSelled = True
                         lp = 1.02
                         sleepTime = 1
-                        btc = get_balance("ETH")
+                        btc = get_balance("BTC")
                         if btc > 0.00008:
-                            upbit.sell_market_order("KRW-ETH", btc*0.9995)
+                            upbit.sell_market_order("KRW-BTC", btc*0.9995)
+                            logger.info("sell BTC")
         else:
             bisSelled = False
             bisFinished = False
             sleepTime = 1
             lp = 1.02
-            btc = get_balance("ETH")
+            btc = get_balance("BTC")
             if btc > 0.00008:
-                upbit.sell_market_order("KRW-ETH", btc*0.9995)
+                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+                logger.info("sell BTC adn New Day")
         time.sleep(sleepTime)
+        
     except Exception as e:
         print(e)
+        logger.info(e)
         time.sleep(1)
