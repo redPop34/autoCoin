4bb01ba2e4b384f5901b0b6d5526a9376b1361b1
redPop34 <83233848+redPop34@users.noreply.github.com>
2021-04-30 17:03:40 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,27 @@
+import time
+import datetime
+import logging
+
+# 로그 생성
+logger = logging.getLogger()
+
+# 로그의 출력 기준 설정
+logger.setLevel(logging.INFO)
+
+# log 출력 형식
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# log 출력
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+# log를 파일에 출력
+file_handler = logging.FileHandler('BTC.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+lp = 1.04
+if lp > 1.02:
+    logger.
+
